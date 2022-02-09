20ec471470585f2a86c19a0c3453dc5063e61542
mdiallo98 <mamadou1237@gmail.com>
2022-02-08 22:17:44 -0500

    deleted a bunch of stuff

--- src/StyledComponents.jsx
+++ src/StyledComponents.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import BasicTitle from './StyledComponents/BasicTitle';
-import { Buttonone, OtherButton } from './StyledComponents/Buttons';
-import { Button } from '@mui/material';
-
-const MUIVariantBtn = styled(Button)`
-  text-transform: capitalize;
-  color: red;
-`;
 
 function StyledComponents() {
-  return (
-    <div>
-      <BasicTitle>Styled Components</BasicTitle>
-      <BasicTitle special>I am the Special one</BasicTitle>
-      <Buttonone>Click Me</Buttonone>
-      <OtherButton>Click me too</OtherButton>
-      <Button color="primary" variant="contained">
-        MUI button
-      </Button>
-      <MUIVariantBtn>I am the variant</MUIVariantBtn>
-    </div>
-  );
+  return <div style={{ padding: '2rem' }}></div>;
 }
 
 export default StyledComponents;
